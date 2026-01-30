782bdc80f16787b3d2a7d54b222ca14942e50ed3
Anachronismica <khadijapetlele@gmail.com>
2026-01-30 12:34:48 +0200

    anim Stuff
    Animation Controller Setup

--- Assets/Scripts/CharacterMovementController.cs
+++ Assets/Scripts/CharacterMovementController.cs
@@ -1,38 +1,53 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class CharacterMovementController : MonoBehaviour
 {
     public PlayerInput playerInput;
-
     public Rigidbody2D rb;
 
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
     [SerializeField] private float moveSpeed;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private Vector2 moveInput;
-
-
-    
 
     public void MoveInput(InputAction.CallbackContext context)
     {
         Debug.Log("Move Input Detected");
+
         if (context.performed || context.canceled)
         {
             moveInput = context.ReadValue<Vector2>();
             Move();
         }
-        
     }
-
 
     public void Move()
     {
         Vector2 direction = SnapTo8Directions(moveInput);
 
         rb.linearVelocity = direction * moveSpeed;
-    }
 
+        // Animator bool: true if moving, false if idle
+        animator.SetBool("isRunning", rb.linearVelocity.sqrMagnitude > 0.01f);
+
+        if (direction.x > 0.01f)
+        {
+            spriteRenderer.flipX = false; // facing right
+        }
+        else if (direction.x < -0.01f)
+        {
+            spriteRenderer.flipX = true; // facing left
+        }
+    }
 
     Vector2 SnapTo8Directions(Vector2 input)
     {
@@ -45,5 +60,4 @@ public class CharacterMovementController : MonoBehaviour
         float rad = angle * Mathf.Deg2Rad;
         return new Vector2(Mathf.Cos(rad), Mathf.Sin(rad));
     }
-
 }
